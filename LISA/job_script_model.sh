9eca623927db82d5493cca363048f9542eda3c12
Fausto Carcassi <fausto.carcassi@gmail.com>
2021-02-17 13:53:13 +0100

    Send files in single loop LISA

--- LISA/job_script_model.sh
+++ LISA/job_script_model.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
 #SBATCH -n 1
 #SBATCH -p shared
-#SBATCH -t 10:00:00
+#SBATCH -t 1:00:00
 
 module load 2019
 module load Python/3.6.6-intel-2019b
 # num_trial bottleneck save_path n_gens n_agents max_model_size n_epochs
 source ../venv/bin/activate
-for i in {1..20}
-do
-	echo($i) & \
-	python ../iteration.py \
-	--num_trial $i \
-	--bottleneck $1 \
-	--save_path ./Archive \
-	--n_generations 300 \
-	--n_agents 10 \
-	--max_model_size 10 \
-	--n_epochs $2 \
-	--shuffle_input False \
-	--optimizer sgd &
-done
+
+python ../iteration.py \
+--num_trial $i \
+--bottleneck $1 \
+--save_path ./Archive \
+--n_generations 300 \
+--n_agents 10 \
+--max_model_size 10 \
+--n_epochs $2 \
+--shuffle_input False \
+--optimizer sgd
+
 deactivate
--- LISA/multiple_runs_model.sh
+++ LISA/multiple_runs_model.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-for bneck in 200 512 715 1024 
+for i in {1..20}
 do
-	for nepoch in 4 8
+	for bneck in 200 512 715 1024 
 	do
-		sbatch job_script_model.sh $bneck $nepoch
+		for nepoch in 4 8
+		do
+			sbatch job_script_model.sh $bneck $nepoch
+		done
 	done
 done
