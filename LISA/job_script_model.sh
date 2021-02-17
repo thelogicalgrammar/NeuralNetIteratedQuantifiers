caab23e99f0c3925ee10204fb5e768ec86ca29ea
Fausto Carcassi <fausto.carcassi@gmail.com>
2021-02-17 13:56:46 +0100

    fix numtrial bug

--- LISA/job_script_model.sh
+++ LISA/job_script_model.sh
@@ -9,13 +9,13 @@ module load Python/3.6.6-intel-2019b
 source ../venv/bin/activate
 
 python ../iteration.py \
---num_trial $i \
---bottleneck $1 \
+--num_trial $1 \
+--bottleneck $2 \
 --save_path ./Archive \
 --n_generations 300 \
 --n_agents 10 \
 --max_model_size 10 \
---n_epochs $2 \
+--n_epochs $3 \
 --shuffle_input False \
 --optimizer sgd
 
--- LISA/multiple_runs_model.sh
+++ LISA/multiple_runs_model.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-for i in {1..20}
+for ntrial in {1..20}
 do
 	for bneck in 200 512 715 1024 
 	do
 		for nepoch in 4 8
 		do
-			sbatch job_script_model.sh $bneck $nepoch
+			sbatch job_script_model.sh $ntrial $bneck $nepoch
 		done
 	done
 done
